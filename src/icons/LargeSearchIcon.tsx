67d4a3fe917cd5193366f4558fd286eeb8b2efeb
Camila Soares <camilagoulartsoares@yahoo.com>
2025-07-28 19:10:35 -0300

    Deleção

--- src/icons/LargeSearchIcon.tsx
+++
@@ -1,6 +0,0 @@
-export const LargeSearchIcon = () => (
-  <svg width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <circle cx="11" cy="11" r="8" />
-    <path d="m21 21-4.35-4.35" />
-  </svg>
-)
--- src/icons/SearchIcon.tsx
+++
@@ -1,6 +0,0 @@
-export const SearchIcon = () => (
-  <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-    <circle cx="11" cy="11" r="8"></circle>
-    <path d="m21 21-4.35-4.35"></path>
-  </svg>
-)
